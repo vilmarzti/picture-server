584808f317e4bed2b85bf6ed9c219814f49af4f1
Villavicencio Martin <villavicencio.martin@protonmail.com>
2020-10-13 16:59:42 +0200

    remove vote-all

--- src/app/vote/vote.module.ts
+++ src/app/vote/vote.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { VoteIdComponent } from './vote-id/vote-id.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { VoteAllComponent } from './vote-all/vote-all.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { VoteInfoComponent } from './vote-info/vote-info.component';
@@ -13,7 +12,7 @@ import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [VoteIdComponent, VoteAllComponent, VoteInfoComponent, VoteByeComponent],
+  declarations: [VoteIdComponent, VoteInfoComponent, VoteByeComponent],
   imports: [
     CommonModule,
     BrowserModule,
