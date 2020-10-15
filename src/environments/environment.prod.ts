5e34a7baacaa43b1936689989f2757ba7e77b266
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-10-16 00:36:55 +0200

    changed to olivia.li

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { common } from './environment.common';
 
 export const environment = {
   production: true,
-  backend_url: "http://18.218.44.61",
+  backend_url: "http://olivia.li",
   ...common
 };
