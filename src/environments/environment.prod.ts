27b6cc74cec3241c475cf97ba6f871fab683da6c
Villavicencio Martin <villavicencio.martin@protonmail.com>
2020-10-15 13:16:59 +0200

    updated ports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  backend_url: "http://18.220.129.153",
+  backend_url: "http://18.218.44.61",
   backend_port: 8000,
   model: {
     seq2seq:{
-      port: 5001
+      port: 8002
     },
     baseline: {
-      port: 5000
+      port: 8001
     }
   }
 };
