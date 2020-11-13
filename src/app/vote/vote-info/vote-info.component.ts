286a7f9f7c085f85c0872346da42f667a7604ba6
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-11-13 14:27:46 +0100

    background-tiles

--- src/app/vote/vote-info/vote-info.component.ts
+++ src/app/vote/vote-info/vote-info.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { timeout } from 'rxjs/operators';
 import { Picture } from 'src/app/shared/picture';
 import { PictureService } from 'src/app/shared/picture.service';
 
@@ -7,13 +8,17 @@ import { PictureService } from 'src/app/shared/picture.service';
   templateUrl: './vote-info.component.html',
   styleUrls: ['./vote-info.component.scss']
 })
-export class VoteInfoComponent implements OnInit, AfterViewInit{
+export class VoteInfoComponent implements OnInit, AfterViewInit {
+
   @ViewChildren('backgroundImage') private _backgroundImages: QueryList<ElementRef>
-  public base_url: string ="/vote";
+
+  public base_url: string = "/vote";
+  public backgroundImagesInit: boolean = true;
   public continue_id: number = 0;
   public starting_id: number = 0;
   public isNaN: Function = isNaN;
-  public pictures: Picture[] = [];
+  public backgroundPictures: Picture[] = [];
+  private _pictures: Picture[] = [];
 
   constructor(
     private pictureService: PictureService
@@ -25,25 +30,55 @@ export class VoteInfoComponent implements OnInit, AfterViewInit{
 
     // get saved id
     this.continue_id = this.pictureService.currentId;
-    // get starting id
-    this.pictureService.getAllPictures().subscribe(
-      pictures =>{
-        if(pictures.length > 0){
-          this.starting_id = pictures[0].id;
-          this.pictures = pictures;
-        }
-      },
-      err =>{
-        console.log(err);
-      }
-    )
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event): void {
+    let html = document.documentElement
+    let body = document.body;
+
+
+    let screen_height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    let screen_width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
+
+    let box_height = this._backgroundImages.first.nativeElement.height;
+    let box_width = this._backgroundImages.first.nativeElement.width;
+    console.log(box_height);
+    console.log(box_width);
+
+    let num_boxes = Math.ceil((screen_height * screen_width) / (box_height * box_width));
+    console.log(num_boxes);
+
+    /*for (let i = 0; i < num_boxes; i++) {
+      this.backgroundPictures.push(this._pictures[i % this._pictures.length])
+    }
+    */
   }
 
   ngAfterViewInit(): void {
-    this._backgroundImages.changes.subscribe( 
-      backgroungImages => {
-        this.pictures.push(this.pictures[0])
-        console.log(this.pictures)
+    // get starting id and load pictures for background
+    this.pictureService.getAllPictures().subscribe(
+      pictures => {
+        if (pictures.length > 0) {
+          this.starting_id = pictures[0].id;
+          this._pictures = pictures;
+          this.backgroundPictures = pictures;
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    )
+
+    this._backgroundImages.changes.subscribe(
+      () => {
+        if (this.backgroundImagesInit) {
+          this.onResize(null);
+        }
+        else {
+          this.backgroundImagesInit = false;
+        }
       }
     )
   }
