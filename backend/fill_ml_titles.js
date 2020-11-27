0158dad2a42047e2550a3e4b560fa5bf0fc1272d
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-11-27 17:15:11 +0100

    small steps

--- backend/fill_ml_titles.js
+++ backend/fill_ml_titles.js
@@ -1,18 +1,25 @@
 const fs = require('fs')
 const path = require('path')
+const http = require('http')
 const mongoose = require('mongoose')
 const mongoOptions = require('./mongoose_options')
 const pictureSchema = require('./schema')
 const { createSVGWindow } = require('svgdom')
+
 
 // Constants
 const port = 8001 // the port where the deeplearning model is listenting
 const svg_path = "./data/SVG" // path to the folder with the svg's
 const step_distance = 3 // at every <step_distance> there is a cut
 const Picture = mongoose.model('Picture', pictureSchema) // the schema with which find and update models
+const window = createSVGWindow()
+const document = window.document
+
+// Initialization of SVG-Utilities
+const { SVG, registerWindow } = require('@svgdotjs/svg.js')
+registerWindow(window, document)
 
 // Functions
-
 // cuts a path into steps with length 
 function cut_path_steps(paths) {
     let cut_paths = []
@@ -46,6 +53,26 @@ function perm(xs) {
     return ret
 }
 
+const permutator = (inputArr) => {
+    let result = [];
+
+    const permute = (arr, m = []) => {
+        if (arr.length === 0) {
+            result.push(m)
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                let curr = arr.slice();
+                let next = curr.splice(i, 1);
+                permute(curr.slice(), m.concat(next))
+            }
+        }
+    }
+
+    permute(inputArr)
+
+    return result;
+}
+
 // Take paths and creates a format that the server expects
 function compose_data_format(paths) {
     let text_object = {
@@ -62,7 +89,7 @@ function compose_data_format(paths) {
                 type = "start"
             }
 
-            if (index === num_cuts - 1) {
+            if (index === path.length) {
                 type = "end"
                 ev = 1
             }
@@ -83,7 +110,7 @@ function compose_data_format(paths) {
 }
 
 // sends the formatted paths to the server
-function send_to_server(text_object, interpretations) {
+async function send_to_server(text_object, interpretations) {
     let options = {
         hostname: "127.0.0.1",
         port: port,
@@ -96,7 +123,7 @@ function send_to_server(text_object, interpretations) {
     };
 
     let body_chunks = []
-    const req = request(
+    const req = http.request(
         options,
         (res) => {
             res.on('data', (chunk) => {
@@ -119,8 +146,40 @@ function send_to_server(text_object, interpretations) {
 }
 
 // process filename
-async function process(file_name) {
+async function process(file_name, file_path) {
+    // read file
+    let svg_text = fs.readFileSync(path.join(file_path, file_name), { encoding: 'utf-8' })
+
+    // create canvas
+    let draw = SVG(document.documentElement)
+
+    // draw SVG
+    let store = draw.svg(svg_text)
+    const cut_paths = cut_path_steps(store.find("path"))
+
+    // get permutations of paths
+    //const permutations = permutator(cut_paths)
+
+    let interpretations = [];
+    const json_format = compose_data_format(cut_paths);
+    send_to_server(json_format, interpretations);
+
     // get the new titles
+    /*
+    let interpretations = []
+    let bool = true;
+    for (const path of permutations) {
+        const json_format = compose_data_format(path)
+        setTimeout(
+            () => {
+                send_to_server(json_format, interpretations)
+            },
+            10
+        )
+    }
+    */
+
+
 
 
     // get picture where the filename is included
@@ -136,17 +195,22 @@ async function process(file_name) {
 }
 
 
-// Main Code execution
-mongoose.connect(mongoOptions.db_path, mongoOptions.options)
-console.log('mongoose connected')
+// Main execution
+async function main() {
+    mongoose.connect(mongoOptions.db_path, mongoOptions.options)
+    console.log('mongoose connected')
 
-// Check if svg_path exists
-if (fs.realpathSync(svg_path)) {
-    // read all the filenames in the given directory
-    const files = fs.readdirSync(svg_path)
+    // Check if svg_path exists
+    if (fs.realpathSync(svg_path)) {
+        // read all the filenames in the given directory
+        const files = fs.readdirSync(svg_path)
 
-    // Go through all files
-    for (const geste of files) {
-        process(geste)
+        // Go through all files
+        for (const geste of files) {
+            process(geste, svg_path)
+        }
     }
 }
+
+// execute code
+main()
