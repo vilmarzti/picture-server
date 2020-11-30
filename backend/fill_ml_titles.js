be65308078369ea06ec4508237c9c48d2ec9cea8
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-11-30 16:15:38 +0100

    removed permutations function

--- backend/fill_ml_titles.js
+++ backend/fill_ml_titles.js
@@ -40,23 +40,6 @@ function cut_path_steps(paths) {
 }
 
 // find all permutations of an array
-function perm(xs) {
-    let ret = []
-
-    for (let i = 0; i < xs.length; i = i + 1) {
-        let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)))
-
-        if (!rest.length) {
-            ret.push([xs[i]])
-        } else {
-            for (let j = 0; j < rest.length; j = j + 1) {
-                ret.push([xs[i]].concat(rest[j]))
-            }
-        }
-    }
-    return ret
-}
-
 const permutator = (inputArr) => {
     let result = [];
 
