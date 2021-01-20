ff4295ccb77f433d9183652aca4485cd179509f0
Villavicencio Martin <villavicencio.martin@protonmail.com>
2021-01-20 08:58:47 +0100

    updated text and added links to interview/pdf

--- src/app/more/more/more.component.ts
+++ src/app/more/more/more.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-more',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoreComponent implements OnInit {
 
+  public _backend_url = new URL(environment.backend_url);
+  public interview_url;
+  public bachelorthesis_url;
+
   constructor() { }
 
   ngOnInit(): void {
+    this._backend_url.port = environment.backend_port.toString();
+    this.interview_url = new URL("/image/video/interview.mp4", this._backend_url)
+    this.bachelorthesis_url = new URL("/image/pdf/bachelorarbeit.pdf", this._backend_url);
   }
 
 }
